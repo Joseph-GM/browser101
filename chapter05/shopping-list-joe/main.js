49a31bc86ae1c87f557f67c57a8f320faaed055a
joseph-GM <jkim.english75@gmail.com>
2022-05-17 08:04:21 +0900

    complete shopping list by myself

--- chapter05/shopping-list-joe/main.js
+++ chapter05/shopping-list-joe/main.js
@@ -1,7 +1,12 @@
 console.log('start')
 
+let itemsArray = ["화장지", "세제", "비누"]
+
 const textInput = document.querySelector('.textInput');
 const addBtn = document.querySelector('.addBtn');
+const itemsList = document.querySelector('.items');
+
+renderList(itemsArray);
 
 textInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
@@ -11,6 +16,33 @@ textInput.addEventListener('keypress', (e) => {
 })
 
 addBtn.addEventListener('click', () => {
+    if (!textInput.value) {
+        return
+    }
     console.log(textInput.value);
+    itemsArray.push(textInput.value);
+    renderList(itemsArray);
     textInput.value = '';
 });
+
+function renderList(items) {
+    let summary = '';
+    items.forEach((element, index) => {
+        console.log(index);
+        summary = summary + `
+        <li class="item">
+            <span>${element}</span>
+            <button class="deleteBtn" onclick="deleteBtnClicked(${index})">
+                <i class="fa-solid fa-trash-can"></i>
+        </button>
+        </li>
+        `
+    });
+    itemsList.innerHTML = summary
+}
+
+function deleteBtnClicked(index) {
+    console.log(`${index} clicked`);
+    itemsArray.splice(index,1);
+    renderList(itemsArray);
+}
