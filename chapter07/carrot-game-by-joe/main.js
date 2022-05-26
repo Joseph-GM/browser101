00ca1768f48f03764df3d54e33e0c0382de616e3
joseph-GM <jkim.english75@gmail.com>
2022-05-27 07:16:40 +0900

    javascript fix for bug and carrot

--- chapter07/carrot-game-by-joe/main.js
+++ chapter07/carrot-game-by-joe/main.js
@@ -4,6 +4,7 @@ let carrotImgElemetnArray = [];
 
 const playBtn = document.querySelector('.play');
 const gameZone = document.querySelector('.gameZone');
+const infoZone = document.querySelector('.infoZone');
 
 playBtn.addEventListener('click', () => {
     console.log("paly Btn clicked;")
@@ -19,23 +20,29 @@ playBtn.addEventListener('click', () => {
 
 function renderBugsAndCarrots() {
     console.log('renderBugsAndCarrots() called')
-    const shape = gameZone.getBoundingClientRect()
-    const width = shape.width;
-    const height = shape.height;
+    const gameZoneShape = gameZone.getBoundingClientRect()
+    const gameZoneWidth = gameZoneShape.width;
+    const gameZoneHeight = gameZoneShape.height;
+
+    const infoZoneShape = infoZone.getBoundingClientRect();
+    const infoZoneWidth = infoZoneShape.width;
+    const infoZoneHeight = infoZoneShape.height;
+   
     for (let i = 0; i < 10; i++) {
 
-        const xBug = getRandomArbitrary(width);
-        const yBug = getRandomArbitrary(height);
-        const xCarrot = getRandomArbitrary(width);
-        const yCarrot = getRandomArbitrary(height);
+        const xBug = getRandomArbitrary(0, gameZoneWidth);
+        const yBug = getRandomArbitrary(infoZoneHeight, (infoZoneHeight+gameZoneHeight));
+        const xCarrot = getRandomArbitrary(0, gameZoneWidth);
+        const yCarrot = getRandomArbitrary(infoZoneHeight, (infoZoneHeight+gameZoneHeight));
 
         const bugElement = document.createElement('img');
         bugElement.setAttribute("class", `bug${i}`);
         bugElement.setAttribute("src", "img/bug.png");
         bugElement.setAttribute("alt", "bug image"); 
-        bugElement.style.position = 'relative';
+        bugElement.style.position = 'absolute';
         bugElement.style.top = `${yBug}px`;
         bugElement.style.left = `${xBug}px`;
+        // bugElement.style.transform = `translate(${xBug}px, ${yBug}px)`;
         gameZone.appendChild(bugElement);
         console.log(`bug${i} cordinate(${xBug}%, ${yBug}%)`)
 
@@ -43,16 +50,17 @@ function renderBugsAndCarrots() {
         carrotElement.setAttribute("class", `carrot${i}`);
         carrotElement.setAttribute("src", "img/carrot.png");
         carrotElement.setAttribute("alt", "carrot image"); 
-        carrotElement.style.position = 'relative';
+        carrotElement.style.position = 'absolute';
         carrotElement.style.top = `${yCarrot}px`;
         carrotElement.style.left = `${xCarrot}px`;
+        // carrotElement.style.transform = `translate(${xCarrot}px, ${yCarrot}px)`;
         gameZone.appendChild(carrotElement);
         console.log(`carrot${i}cordinate(${xCarrot}%, ${yCarrot}%)`)
 
     }
 }
 
-function getRandomArbitrary(width) {
-    return Math.random() * (width - 20) + 20;
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
   }
 
